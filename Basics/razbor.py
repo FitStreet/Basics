85dd80a8e8468dfdf620c4d1bdab957fd949e1d5
FitStreet <fitstreety@gmail.com>
2023-10-06 19:20:23 +0600

    add File(Test)

---
+++ Basics/razbor.py
@@ -0,0 +1,10 @@
+a = 'b'
+if a in "sadsafrga": # все что после If переводится в bool type(true or false)
+    print("yes")
+
+str = "hello world i am alive"
+counter = 0
+for i in str:
+    if i in 'auioye':
+        counter += 1
+print(counter)
