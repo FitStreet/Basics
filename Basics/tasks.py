e2246ae3ddc36d3e55c1a8b4e38786e7763968f4
FitStreet <fitstreety@gmail.com>
2023-10-11 11:21:04 +0600

    Tasks

---
+++ Basics/tasks.py
@@ -0,0 +1,44 @@
+# sets task 13
+
+# robert = {5, 7, 11, 10, 28} 
+# kail = {1, 5, 14, 8, 22} 
+# merri = {19, 20, 3, 11, 10}
+
+# kail_guess = len(robert.intersection(kail)) > 0
+# merri_guess = len(robert.intersection(merri)) > 0
+
+# if kail_guess and merri_guess:
+#     print("kail merri")
+# elif kail_guess:
+#     print("kail")
+# elif merri_guess:
+#     print("merri")
+# else:
+#     print("no one")
+
+#task 14
+
+# tilek = {'Dodo', "imperiaPizza", "FreshBox"}
+# timur = {'OchakKebab', "FreshBox"}
+# alexander = {"FreshBox", "KFC"}
+# elina = {"Mac", "Subway"}
+# elina |= tilek
+# elina |= timur
+# elina |= alexander
+
+# tilek.intersection_update(timur)
+# tilek.intersection_update(alexander)
+# tilek.intersection_update(elina)
+
+# print(tilek)
+
+# ingredients = {"cыр чеддар","грибы", "соус","шпинат"} 
+
+# ingredients.add("помидор")
+# ingredients.discard("колбаса")
+# ingredients.remove("шпинат")
+# ingredients.add("базилик")
+# ingredients.remove("cыр чеддар")
+# ingredients.add("сыр моцарелла")
+
+# print(ingredients)
