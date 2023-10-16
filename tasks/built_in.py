12a8582c51f01146b04c11c7d5683b333b405a41
FitStreet <fitstreety@gmail.com>
2023-10-16 21:28:30 +0600

    .

---
+++ tasks/built_in.py
@@ -0,0 +1,30 @@
+"""Task 1"""
+# list_ = [1, 2, 3, 4]  
+# import functools
+# result = functools.reduce(lambda x,y: x+y,list_)
+# print(result)
+"""Task 2"""
+# list_ = [5,5,5] 
+# result = all(i > 3 for i in list_)
+# print(result)
+"""Task 3"""
+# list_ = [5, 8, 4, 6, 7]
+# result = any(i < 0 for i in list_)
+# print(result)
+"""Task 4"""
+# list_ = [1, 2, 3, 4]  
+# result = list(map(lambda x: x ** 2, list_))
+# print(result)
+"""Task 5"""
+# list_ = [1, 2, 3, 4] 
+# result = list(filter(lambda x: x % 2 ==0, list_))
+# print(result)
+"""Task 6"""
+# list_ = ['inheritance', 'solid', 'polymorphism', 'dry', 'yagni',] 
+# result = list(filter(lambda x: len(x) > 7, list_ ))
+# print(result)
+"""Task 7"""
+list_ = [5, 6, 7, 8] 
+import functools
+result = functools.reduce(lambda x,y: x*y,list_)
+print(result)
