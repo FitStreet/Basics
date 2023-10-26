9da214dd5b7f02ae74007895034f362dff22c695
FitStreet <fitstreety@gmail.com>
2023-10-26 23:02:15 +0600

    Files Tasks

--- files/ertay.py
+++ files/ertay.py
@@ -1,1 +1,13 @@
-print("hello world")
+# with open("task4.txt", 'r') as file:
+#     print(len(file.readlines()))
+
+# with open('task5.txt', 'r') as file:
+#     list1 = list(map(int,file.readlines()))
+#     minimal = min(list1)
+#     maximum = max(list1)
+
+# with open('task6.txt', 'x') as new_file:
+#     new_file.write(f'{minimal}-{maximum}')
+
+
+    
